41d348d799983e8e84ed525ac5bd7bb5d17489e1
dev-strenuus <andrea5.scotti@mail.polimi.it>
2019-10-23 18:05:59 +0200

    log messages

--- beliefPropagationSimulation/factorTreeGenerator.py
+++ beliefPropagationSimulation/factorTreeGenerator.py
@@ -1,16 +1,17 @@
 import numpy as np
 import random
 
-def generateRandomDistribution():
+def generateRandomDistribution(length):
     sum = 0
-    v = [0]*8
-    for i in range(8):
-        v[i] = random.randint(1,100)
-        sum = sum + v[i]
-    for i in range(8):
-        v[i] = v[i] / sum
-    return v[i]
-n = 100
+    v = [0]*length
+    for i in range(length):
+        v[i] = random.randint(1, 3)
+    """    sum = sum + v[i]
+    for i in range(length):
+        v[i] = v[i] / sum"""
+    return v
+
+n = 100000
 test = 0
 
 q = []
@@ -28,13 +29,13 @@ while(len(q) > 0):
 graph = [ [] for i in range(n)]
 mapping = [-1]*n
 
-def renameVariable(node, type, cont):
+def renameVariable(node, type, newId):
     if type == 0:
-        mapping[node] = cont
-        cont = cont + 1
-    greatest = cont
+        mapping[node] = newId
+        newId = newId + 1
+    greatest = newId
     for child in g[node]:
-        greatest = max(greatest, renameVariable(node, 1-type, cont))
+        greatest = max(greatest, renameVariable(child, 1-type, greatest))
     return greatest
 
 
@@ -53,5 +54,18 @@ for i in range(n):
         graph[mapping[i]].append(mapping[j])
         graph[mapping[j]].append(mapping[i])
 
-fp = open("input"+str(test)+".txt", "w+")
-fp.write()
+fp = open("../data/input"+str(test)+".txt", "w+")
+fp.write(str(variablesNumber)+ " " + str(factorsNumber) + "\n")
+for i in range(factorsNumber):
+    for j in graph[variablesNumber+i]:
+        fp.write(str(j))
+        if j != graph[variablesNumber+i][len(graph[variablesNumber+i])-1]:
+            fp.write(" ")
+    fp.write("\n")
+    temp = generateRandomDistribution(2**len(graph[variablesNumber+i]))
+    for j in range(2**len(graph[variablesNumber+i])):
+        fp.write(str(temp[j]))
+        if j != 2**len(graph[variablesNumber+i]) - 1:
+            fp.write(" ")
+    fp.write("\n")
+fp.close()
